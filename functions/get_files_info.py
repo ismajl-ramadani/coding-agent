1d6b4fb2839626c5a9ef8688f2604394a2148b07
Ismajl Ramadani <helloismajl@gmail.com>
2026-04-07 09:10:26 +0200

    Add 'working_directory' to get_files_info schema

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -38,6 +38,11 @@ schema_get_files_info = types.FunctionDeclaration(
                 type=types.Type.STRING,
                 description="Directory path to list files from, relative to the working directory (default is the working directory itself)",
             ),
+            "working_directory": types.Schema(
+                type=types.Type.STRING,
+                description="Directory path to list files from, relative to the working directory (default is the working directory itself)",
+            ),
         },
+        required=["working_directory"],
     ),
 )
